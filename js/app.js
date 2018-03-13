224017869a2d88d2133bbb8f1f9a38f1927cfe8a
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-03-13 15:10:28 -0700

    got some functionaliy going and even some css for my table

--- js/app.js
+++ js/app.js
@@ -1,6 +1,75 @@
 'use strict';
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var allStores = [];
+var storeTable = document.getElementById('stores');
+
+function Store (location, minCust, maxCust, avgCookiePerSale) {
+  this.location = location;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookiePerSale = avgCookiePerSale;
+  this.totalCookies = 0;
+  allStores.push(this);
+}
+
+Store.prototype.render = function() {
+  // create tr
+  var trElement = document.createElement('tr');
+
+  // create th element
+  var thElement = document.createElement('th');
+
+  // create td
+  var tdElement = document.createElement('td');
+
+  // give th content
+  thElement.textContent = this.location;
+
+  // append th to tr
+  trElement.appendChild(thElement);
+
+  // make td for other fields
+  for (var i = 0; i < hours.length; i++) {
+    this.hourlyCookies = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust) * this.avgCookiePerSale);
+    tdElement = document.createElement('td');
+    tdElement.textContent = this.hourlyCookies;
+    trElement.appendChild(tdElement);
+    this.totalCookies += this.hourlyCookies;
+  }
+  storeTable.appendChild(trElement);
+
+};
+
+var firstAndPike = new Store('1st and Pike', 23, 65, 6.3);
+var seaTac = new Store('SeaTac', 3, 24, 1.2);
+var seaCent = new Store('Seattle Center', 11, 38, 3.7);
+
+firstAndPike.render();
+seaTac.render();
+seaCent.render();
+/*
+  this.hourlyCookies = function() {
+    return Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust) * this.avgCookiePerSale);
+  };  
+  this.showSales = function() {
+    var shopContainer = document.getElementById('shop1');
+    var totalCookies = 0;
+    for (var i = 0; i < hours.length; i++) {
+      var hourElement = document.createElement('li');
+      var currentHourCookies = this.hourlyCookies();
+      hourElement.textContent = hours[i] + ': ' + currentHourCookies;
+      shopContainer.appendChild(hourElement);
+      totalCookies += currentHourCookies;
+    }
+    var totalElement = document.createElement('li');
+    totalElement.textContent = 'Total: ' + totalCookies;
+    shopContainer.appendChild(totalElement);
+  };*/
+/*
+var firstAndPike = new Store('1st and Pike', 23, 65, 6.3);
+firstAndPike.hourlyCookies();
+firstAndPike.showSales();
 
 var pikeShop = {
   location: '1st and Pike',
@@ -131,8 +200,9 @@ var alkiShop = {
   }
 };
 
-pikeShop.showSales();
+//pikeShop.showSales();
 seaTacShop.showSales();
 seattleCenterShop.showSales();
 capitolHillShop.showSales();
 alkiShop.showSales();
+*/
