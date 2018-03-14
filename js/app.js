c94b205aa3e63a34b1b199b5bc94412369ad92eb
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-03-14 14:05:02 -0700

    some js functionality with adding with a form, but still clunky

--- js/app.js
+++ js/app.js
@@ -1,19 +1,28 @@
 'use strict';
 
+// Access the table that is in the DOM
+var storeTable = document.getElementById('stores');
+
+// Access the form so we can attach an event listener
+var storeForm = document.getElementById('store-form');
+
+// A few variables we're going to delcare in the beginning
 var storeHours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 var allStores = [];
-var storeTable = document.getElementById('stores');
 var totalOfTotals = 0;
-function Store (location, minCust, maxCust, avgCookiePerSale) {
+
+// A constructor for our store objects
+function Store (location, minCust, maxCust, avgCookiesPerSale) {
   this.location = location;
   this.minCust = minCust;
   this.maxCust = maxCust;
-  this.avgCookiePerSale = avgCookiePerSale;
+  this.avgCookiesPerSale = avgCookiesPerSale;
   this.totalCookies = 0;
   this.hourlyCookiesArray = [];
   allStores.push(this);
 }
 
+// Render the stores in the table
 Store.prototype.render = function() {
   // create tr
   var trElement = document.createElement('tr');
@@ -33,7 +42,7 @@ Store.prototype.render = function() {
 
   // make td for other fields
   for (var i = 0; i < storeHours.length; i++) {
-    this.hourlyCookiesCalc = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust) * this.avgCookiePerSale);
+    this.hourlyCookiesCalc = Math.floor((Math.random() * (this.maxCust - this.minCust) + this.minCust) * this.avgCookiesPerSale);
     tdElement = document.createElement('td');
     tdElement.textContent = this.hourlyCookiesCalc;
     this.hourlyCookiesArray.push(this.hourlyCookiesCalc);
@@ -48,6 +57,12 @@ Store.prototype.render = function() {
 
   storeTable.appendChild(trElement);
 };
+
+new Store('1st and Pike', 23, 65, 6.3);
+new Store('SeaTac', 3, 24, 1.2);
+new Store('Seattle Center', 11, 38, 3.7);
+new Store('Capitol Hill', 20, 38, 2.3);
+new Store('Alki', 2, 16, 4.6);
 
 function makeHeaderRow() {
   var headerTrElement = document.createElement('tr');
@@ -96,16 +111,45 @@ function makeTotalsRow() {
   storeTable.appendChild(totalTrElement);
 }
 
+function renderAllStores() {
+  for(var i in allStores) {
+    allStores[i].render();
+  }
+}
+
+function addNewStore(event) {
+  event.preventDefault();
+  var newLoc = event.target.storeLoc.value;
+  var newMinCust = event.target.minCust.value;
+  var newMaxCust = event.target.maxCust.value;
+  var newAvgCookiesPerSale = event.target.avgCookiesPerSale.value;
+
+  new Store(newLoc, newMinCust, newMaxCust, newAvgCookiesPerSale);
+
+  storeTable.innerHTML = '';
+  makeHeaderRow();
+  renderAllStores();
+  makeTotalsRow();
+}
+/*
 var firstAndPike = new Store('1st and Pike', 23, 65, 6.3);
 var seaTac = new Store('SeaTac', 3, 24, 1.2);
 var seaCent = new Store('Seattle Center', 11, 38, 3.7);
 var capHill = new Store('Capitol Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
+*/
 
+// add event listener
+storeForm.addEventListener('submit', addNewStore);
+
+// call functions
 makeHeaderRow();
+renderAllStores();
+/*
 firstAndPike.render();
 seaTac.render();
 seaCent.render();
 capHill.render();
 alki.render();
+*/
 makeTotalsRow();
